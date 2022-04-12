f57e142b3a91b3a7f58153df79af08d1fe482b53
Pavel <stodumoff@yandex.ru>
2022-04-12 13:03:22 +0300

    graphene is working. very intresting

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -9,6 +9,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from graphene_django.views import GraphQLView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -36,6 +37,9 @@ urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
     path('api/jwt_token/refresh/', TokenRefreshView.as_view()),
     path('api/jwt_token/', MyTokenObtainPairView.as_view()),
+
+    # GraphQl
+    path('graphql/', GraphQLView.as_view(graphiql=True)),
 
     # Documentation
     re_path(r'^swagger(?P<format>\.json|\.yaml)$',
