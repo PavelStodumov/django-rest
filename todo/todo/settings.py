2a44bfc78c89c3e5e7697d664d9009f0686e9322
Pavel <stodumoff@yandex.ru>
2022-04-21 11:31:59 +0300

    q

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -95,18 +95,18 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'drf_todo',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'drf_todo',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'drf_todo',
+    #     'USER': 'admin',
+    #     'PASSWORD': 'admin',
+    #     'HOST': 'drf_todo',
+    #     'PORT': '5432',
+    # }
 }
 
 
